2e323d08f39dff883082136a33a1ec7dbe456a23
Anukarsh <anukarshr@gmail.com>
2021-09-21 11:09:02 +0530

    update

---
+++ script.js
@@ -0,0 +1,45 @@
+console.log("This is a CV-Screener")
+
+// data is an Array of Objects which contains information about the candidates
+const data = [
+    {
+        name: 'Anukarsh Raj',
+        age: 18,
+        city: 'Noida',
+        language: 'C++ | JavaScript',
+        framework: 'Django',
+        image: 'https://randomuser.me/api/portraits/men/75.jpg'
+    },
+    {
+        name: 'Aayushi Srivastav',
+        age: 18,
+        city: 'Noida',
+        language: 'JavaScript',
+        framework: 'React',
+        image: 'https://randomuser.me/api/portraits/men/23.jpg'
+    },
+    {
+        name: 'Satwik Dwivedi',
+        age: 18,
+        city: 'Noida',
+        language: 'Hindi | English',
+        framework: 'IAS',
+        image: 'https://randomuser.me/api/portraits/men/89.jpg'
+    },
+    {
+        name: 'Lucky Raja',
+        age: 18,
+        city: 'Noida',
+        language: 'C++ | Python',
+        framework: 'Django',
+        image: 'https://randomuser.me/api/portraits/men/67.jpg'
+    },
+    {
+        name: 'Divyam Garg',
+        age: 18,
+        city: 'Noida',
+        language: 'C++ | JavaScript',
+        framework: 'Angular',
+        image: 'https://randomuser.me/api/portraits/men/27.jpg'
+    }
+]
